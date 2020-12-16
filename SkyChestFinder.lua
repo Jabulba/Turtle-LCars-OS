de0066c428b72d511e31f69a7d2e09443001535f
Caio Jabulka <Jabulba@users.noreply.github.com>
2020-12-15 23:44:17 -0300

    Update SkyChestFinder.lua

--- SkyChestFinder.lua
+++ SkyChestFinder.lua
@@ -1,13 +1,13 @@
 -- SkyChest search program by Caio "Jabulba" Jabulka 2015
 -- Turtle type: Mining Turtle
 -- Purpose: Use this program to find hidden meteores from Applied Energistics 2.
---			The turtle will dig down until it finds the chest or bedrock
---			if the chest is found chest the turtle will empty it, pick it up and retur to the surface
---			if bedrock is found the turtle returns to the surface, if it broke skystone it will print a warning
+--          The turtle will dig down until it finds the chest or bedrock
+--          if the chest is found chest the turtle will empty it, pick it up and retur to the surface
+--          if bedrock is found the turtle returns to the surface, if it broke skystone it will print a warning
 -- Licensed MIT
 
-local skyChestId = "appliedenergistics2:tile.BlockSkyChest"
-local skyStoneId = "appliedenergistics2:tile.BlockSkyStone"
+local skyChestId = "appliedenergistics2:sky_stone_chest"
+local skyStoneId = "appliedenergistics2:sky_stone_block"
 local bedrockId = "minecraft:bedrock"
 local movesDone = 0
 local skyChestFound = false
@@ -15,75 +15,71 @@ local skyStoneFound = false
 
 local function placeFloor(slot)
 	slot = slot or 1
-	
+
 	if slot > 16 then
 		return true
 	end
-	
+
 	if turtle.getItemCount(slot) > 0 then
 		turtle.select(slot)
 		if not turtle.placeDown() then
 			placeFloor(slot + 1)
 		end
 	end
-	
+
 	return true
 end
 
 local function digMove(direction, attempts)
 	direction = direction or "forward"
 	attempts = attempts or 1
-	
+
 	if attempts > 30 then
 		return false
 	end
-	
+
 	if direction == "f" or direction == "forward" then
 		turtle.dig()
-		
+
 		if not turtle.forward() then
 			turtle.attack()
 			digMove("f", attempts)
 		end
 	elseif direction == "d" or direction == "down" then
 		turtle.digDown()
-		
+
 		if not turtle.down() then
 			turtle.attackDown()
 			digMove("d", attempts)
 		end
 	elseif direction == "u" or direction == "up" then
 		turtle.digUp()
-		
+
 		if not turtle.up() then
 			turtle.attackUp()
 			digMove("u", attempts)
 		end
 	end
-	
+
 	return true
 end
 
 while not skyChestFound do
 	local hasBlock, blockData = turtle.inspectDown()
-	
+
 	if hasBlock and blockData.name == skyChestId then
 		while turtle.suckDown() do
 			print("Picking item from sky chest")
 		end
-		
+
 		turtle.dig()
 		skyChestFound = true
 	elseif hasBlock and blockData.name == skyStoneId and not skyStoneFound then
 		skyStoneFound = true
-	elseif hasBlock and blockData.name == bedrockId then
+	elseif hasBlock then
 		if skyStoneFound then
-			print("========================")
-			print("SKYSTONE HAS BEEN FOUND!")
-			print("========================")
+			break
 		end
-		
-		skyChestFound = true
 	else
 		if digMove("down") then
 			movesDone = movesDone + 1
@@ -95,7 +91,7 @@ while movesDone > 0 do
 	if digMove("up") then
 		movesDone = movesDone -1
 	end
-	
+
 	if movesDone < 5 then
 		placeFloor()
 	end
